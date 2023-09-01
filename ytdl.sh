36d1247a370f51109cd10ebd210600cd35414aab
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-09-01 23:07:04 +0330

    update ytdl.sh

--- ytdl.sh
+++ ytdl.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 
-# Check if Python is installed
-if ! command -v python3 &>/dev/null; then
-    echo "Python is not installed. Installing Python..."
-    sudo apt-get update
-    sudo apt-get install python3 -y
+# Check if the operating system is Windows
+if [ "$OSTYPE" == "msys" ]; then
+    # Windows OS detected
+    python_cmd="python"
 else
-    echo "Python is already installed."
+    # Assume non-Windows (Linux/Mac) OS
+    python_cmd="python3"
+fi
+
+# Check if Python is installed
+if ! command -v $python_cmd &>/dev/null; then
+    echo "$python_cmd is not installed. Installing $python_cmd..."
+    # Install Python based on the detected command (python or python3)
+    if [ "$OSTYPE" == "msys" ]; then
+        # Windows OS
+        choco install python -y
+    else
+        # Linux/Mac OS
+        sudo apt-get update
+        sudo apt-get install $python_cmd -y
+    fi
+else
+    echo "$python_cmd is already installed."
+fi
+
+# Install pytube Python package
+echo "Installing pytube..."
+$python_cmd -m pip install pytube
+
+# Clear the terminal based on the OS
+if [ "$OSTYPE" == "msys" ]; then
+    # Windows OS
+    echo "Clearing the terminal (Windows)..."
+    cls
+else
+    # Linux/Mac OS
+    echo "Clearing the terminal (Linux/Mac)..."
+    clear
 fi
 
 # Download bot.py from the external link
@@ -14,4 +45,5 @@ echo "Downloading bot.py..."
 curl -O https://raw.githubusercontent.com/Ptechgithub/configs/main/bot.py
 
 # Run the bot.py script
-python3 bot.py
+echo "Running bot.py with $python_cmd..."
+$python_cmd bot.py
