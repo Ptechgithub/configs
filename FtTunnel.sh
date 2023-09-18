26390c88984822fdbb50cde569f758817b05de84
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-09-19 01:51:07 +0330

    update FtTunnel.sh

--- FtTunnel.sh
+++ FtTunnel.sh
@@ -39,7 +39,7 @@ configure_arguments() {
 
     if [ "$server_choice" == "2" ]; then
         read -p "Please Enter Password (Please choose the same password on both servers): " password
-        arguments="--server --lport:443 --toip:127.0.0.1 --toport:multiport --password:$password --sni:$sni --terminate:24"
+        arguments="--server --lport:443 --toip:127.0.0.1 --toport:multiport --password:$password --multiport --sni:$sni --terminate:24"
     elif [ "$server_choice" == "1" ]; then
         read -p "Please Enter (Kharej IP) : " server_ip
         read -p "Please Enter Password (Please choose the same password on both servers): " password
@@ -146,7 +146,7 @@ clear
 echo "By --> Peyman * Github.com/Ptechgithub * "
 echo "Your IP is: ($myip) "
 echo ""
-echo " --0------#- Fake Tls Tunnel -#--------"
+echo " --------#- Fake Tls Tunnel -#--------"
 echo "1) Install (Multiport)"
 echo "2) Uninstall (Multiport)"
 echo " ----------------------------"
