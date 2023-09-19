c219e892a7b385d4711df0f98acb85d54e8e0878
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-09-19 03:36:30 +0330

    update FtTunnel.sh

--- FtTunnel.sh
+++ FtTunnel.sh
@@ -47,12 +47,12 @@ configure_arguments2() {
     sni=${sni:-google.com}
 
     if [ "$server_choice" == "2" ]; then
-        read -p "Please Enter Password (Please choose the same password on both servers): " password
-        arguments="--server --lport:443 --toip:127.0.0.1 --toport:$port --password:$password --sni:$sni --terminate:24"
-    elif [ "$server_choice" == "1" ]; then
         read -p "Please Enter (Kharej IP) : " server_ip
         read -p "Please Enter Password (Please choose the same password on both servers): " password
-        arguments="--tunnel --lport:$port --toip:$server_ip --toport:443 --password:$password --sni:$sni --terminate:24"
+        arguments="--kharej --iran-ip:$server_ip --iran-port:443 --toip:127.0.0.1 --toport:$port --password:$password --sni:$sni --terminate:24"
+    elif [ "$server_choice" == "1" ]; then
+        read -p "Please Enter Password (Please choose the same password on both servers): " password
+        arguments="--iran  --lport:443  --sni:$sni --password:$password --terminate:24"
     else
         echo "Invalid choice. Please enter '1' or '2'."
         exit 1
