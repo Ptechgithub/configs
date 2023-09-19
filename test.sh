86b86cdf6a82a94e4092a28b64624b31bccd76b0
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-09-19 14:18:39 +0330

    update test.sh

--- test.sh
+++ test.sh
@@ -1,21 +1,45 @@
 #!/bin/bash
 
+root_access() {
+    # Check if the script is running as root
+    if [ "$EUID" -ne 0 ]; then
+        echo "This script requires root access. please run as root."
+        exit 1
+    fi
+}
+
 # Function to check if wget is installed, and install it if not
 check_dependencies() {
-    if ! command -v wget &> /dev/null; then
-        echo "wget is not installed. Installing..."
-        sudo apt-get install wget
+    local dependencies=("wget" "lsof" "unzip" "iptables")
+
+    if [ -f /etc/os-release ]; then
+        source /etc/os-release
+        case $ID in
+            debian|ubuntu)
+                package_manager="apt-get"
+                ;;
+            centos|rhel)
+                package_manager="yum"
+                ;;
+            fedora)
+                package_manager="dnf"
+                ;;
+            *)
+                echo "Unsupported distribution: $ID"
+                return 1
+                ;;
+        esac
+    else
+        echo "Cannot detect the operating system."
+        return 1
     fi
-    
-    if ! command -v lsof &> /dev/null; then
-        echo "lsof is not installed. Installing..."
-        sudo apt-get install lsof
-    fi
-    
-    if ! command -v iptables &> /dev/null; then
-        echo "iptables is not installed. Installing..."
-        sudo apt-get install iptables
-    fi
+
+    for dep in "${dependencies[@]}"; do
+        if ! command -v "$dep" &> /dev/null; then
+            echo "$dep is not installed. Installing..."
+            sudo "$package_manager" install "$dep"
+        fi
+    done
 }
 
 #Check installed service
