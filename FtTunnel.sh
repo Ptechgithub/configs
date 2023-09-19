d6c19cb4b9cee82803b51f15ce1e12bc28c523c9
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-09-19 12:12:29 +0330

    update FtTunnel.sh

--- FtTunnel.sh
+++ FtTunnel.sh
@@ -251,7 +251,7 @@ clear
 echo "By --> Peyman * Github.com/Ptechgithub * "
 echo "Your IP is: ($myip) "
 echo ""
-echo " --------#- Fake Tls Tunnel -#--------"
+echo " -7-------#- Fake Tls Tunnel -#--------"
 echo "1) Install (Single port)"
 echo "2) Uninstall (Single port)"
 echo " ----------------------------"
