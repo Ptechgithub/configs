e9d30093b44662e71bcbf7e64b91e512de9238a0
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-09-27 21:56:25 +0330

    update RtTunnel.sh

--- RtTunnel.sh
+++ RtTunnel.sh
@@ -408,7 +408,7 @@ stop_tunnel() {
 check_tunnel_status() {
     # Check the status of the tunnel service
     if sudo systemctl is-active --quiet tunnel.service; then
-        echo "Multiport is: [running]"
+        echo "Multiport is:[running]"
     else
         echo "Multiport is:[Not running]"
     fi
@@ -435,9 +435,9 @@ stop_lb_tunnel() {
 check_lb_tunnel_status() {
     # Check the status of the load balancer tunnel service
     if sudo systemctl is-active --quiet lbtunnel.service; then
-        echo "Load balancer is:[ running]"
+        echo "Load balancer is:[running]"
     else
-        echo "Load balancer is:[ Not running]"
+        echo "Load balancer is:[Not running]"
     fi
 }
 
