72140afb90fcdbcbb0f329b6b9c28dd82962c807
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-10-04 00:34:06 +0330

    update gost.sh

--- gost.sh
+++ gost.sh
@@ -104,7 +104,7 @@ questions2() {
         read -p "Please Enter your Config Port : " config_port
         read -p "Enter 'udp' for UDP connection (default is: tcp): " connection_type
         connection_type=${connection_type:-tcp}
-        argument = "-L $connection_type://:$config_port/127.0.0.1:$config_port -F relay+kcp://$foreign_ip:$port"
+        argument="-L $connection_type://:$config_port/127.0.0.1:$config_port -F relay+kcp://$foreign_ip:$port"
         
     elif [ "$server_choice" == "2" ]; then
         read -p "Enter Port : " port
