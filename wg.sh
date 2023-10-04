ab605fc0e26949b411a4eba3db374da64f26c6f0
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-10-04 23:31:24 +0330

    update wg.sh

--- wg.sh
+++ wg.sh
@@ -47,8 +47,6 @@ install_docker_compose() {
         echo "Docker Compose is not installed. Installing..."
         sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
         sudo chmod +x /usr/local/bin/docker-compose
-    else
-        echo "Docker Compose is installed."
     fi
 }
 
