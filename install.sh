429b1cd50c9322327d3476ebbcc492d91ab9f79f
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-10-04 16:27:29 +0330

    update install.sh

--- install.sh
+++ install.sh
@@ -354,7 +354,7 @@ uninstall() {
 
 # Main menu 
 clear
-echo "By --> Peyman * Github.com/Ptechgithub * "
+echo "By -1-> Peyman * Github.com/Ptechgithub * "
 echo ""
 echo " --------#- Go simple Tunnel-#--------"
 echo "1) Install Gost [only Internal Server]"
