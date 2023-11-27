0b97cd1ce0e6221c44f0ac297503232eb12afadd
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-11-27 22:34:15 +0330

    update xy.sh

--- xy.sh
+++ xy.sh
@@ -24,7 +24,7 @@ check_dependencies() {
 #Download Xray Core
 download-xray() {
     if [ ! -f "xy-fragment/xray" ]; then
-        apt update -y
+        pkg update -y
         check_dependencies
         mkdir xy-fragment && cd xy-fragment
         wget https://github.com/XTLS/Xray-core/releases/download/v1.8.4/Xray-android-arm64-v8a.zip
