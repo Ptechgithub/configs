ea7e72be91387b9ec45bbcfdde83a826a1d65be4
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-11-27 21:33:57 +0330

    update xy.sh

--- xy.sh
+++ xy.sh
@@ -98,9 +98,10 @@ EOL
 
 #Install
 install() {
-    clear
     download-xray
     uuid=$(./xray uuid)
+    sleep 1
+    clear
     read -p "Enter a Port between [1024 - 65535]: " port
     [ -f ~/xy-fragment/vmess.txt ] && rm ~/xy-fragment/vmess.txt
     [ -f ~/xy-fragment/vmess.txt ] && rm ~/xy-fragment/config.json
