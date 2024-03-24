78d9d1cc94cf230001959fab2fd3ab7792fcaa29
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2024-03-24 12:04:38 +0330

    update install.sh

--- install.sh
+++ install.sh
@@ -210,13 +210,13 @@ endipresult() {
         echo -e "${purple}****************************************${rest}"
         echo -e "${yellow}          Best IPv4:Port${rest}"
         echo ""
-        echo -e "${yellow}          ${cyan}$Endip_v4 ${rest}"
+        echo -e "${yellow}          ${cyan}$Endip_v4${rest}"
         echo -e "${purple}****************************************${rest}"
     elif [ "$Endip_v6" ]; then
         echo -e "${purple}****************************************${rest}"
-        echo -e "${yellow}          Best IPv6:Port  ${rest}"
+        echo -e "${yellow}          Best IPv6:Port${rest}"
         echo ""
-        echo -e "${yellow}          ${cyan}$Endip_v6 ${rest}"
+        echo -e "${yellow}          ${cyan}$Endip_v6${rest}"
         echo -e "${purple}****************************************${rest}"
     else
         echo -e "${red} No valid IP addresses found.${rest}"
