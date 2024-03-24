1a963667cfa7045575cbb59ee251c2f89a3db381
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2024-03-24 12:57:31 +0330

    update install.sh

--- install.sh
+++ install.sh
@@ -208,18 +208,21 @@ endipresult() {
     cat result.csv | awk -F, '$3!="timeout ms" {print} ' | sort -t, -nk2 -nk3 | uniq | head -11 | awk -F, '{print "Endpoint "$1" Packet Loss Rate "$2" Average Delay "$3}'
     Endip_v4=$(cat result.csv | grep -oE "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+" | head -n 1)
     Endip_v6=$(cat result.csv | grep -oE "\[.*\]:[0-9]+" | head -n 1)
+    delay=$(cat result.csv | grep -oE "[0-9]+ ms|timeout" | head -n 1)
     echo ""
     echo -e "${green}Results Saved in result.csv${rest}"
     echo ""
     if [ "$Endip_v4" ]; then
         echo -e "${purple}************************************${rest}"
         echo -e "${yellow}            Best IPv4:Port${rest}"
+        echo -e "${yellow}            ${green}Delay: $delay${rest}"
         echo ""
         echo -e "${yellow}          ${cyan}$Endip_v4${rest}"
         echo -e "${purple}************************************${rest}"
     elif [ "$Endip_v6" ]; then
         echo -e "${purple}******************************************${rest}"
         echo -e "${yellow}          Best IPv6:Port${rest}"
+        echo -e "${yellow}            ${green}Delay: $delay${rest}"
         echo ""
         echo -e "${yellow} ${cyan}$Endip_v6${rest}"
         echo -e "${purple}******************************************${rest}"
