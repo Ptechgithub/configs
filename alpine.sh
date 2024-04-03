3be7f2c1a121a060e6404bffdf7327b7e2876389
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2024-04-04 01:44:01 +0330

    update alpine.sh

---
+++ alpine.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Download SmartDNS installation package
+mkdir smartDNS
+wget -O smartDNS/smartdns https://github.com/pymumu/smartdns/releases/download/Release45/smartdns-x86_64
+chmod +x smartDNS/smartdns
+
+cat <<EOL > smartDNS/smartdns.conf
+# set listen port
+bind []:53
+# set upstream servers
+server 1.1.1.1
+server-tls 8.8.8.8
+# set domain rules
+address /intel.com/13.91.95.74
+EOL
+
+# Stop and disable existing DNS resolver
+rc-service dnsmasq stop
+#rc-update del dnsmasq
+
+# Start SmartDNS service
+/smartDNS/smartdns -c smartdns.conf
